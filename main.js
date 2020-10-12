9042fc7b5e1b5550b462adcebd3a19bc9431ad86
palash201 <72576747+palash201@users.noreply.github.com>
2020-10-12 18:19:33 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -98,80 +98,85 @@ var gameData = {
             nameString: "dino5"
         }
     }
-}
+};
 
-var enemyNames = ["Hungry Ooze", "Dead Glop", "Gooey Phantom", "Slimy Ogre"]
+var enemyNames = ["Hungry Ooze", "Dead Glop", "Gooey Phantom", "Slimy Ogre"];
 
 var bossNames = {
     oozingForest: ["Shrek", "Mystical Slime", "Starving Ooze"]
-}
+};
 
 
 function update() {
-    document.getElementById("bones").innerHTML = Math.floor(gameData.bones) + " Bones"
-    document.getElementById("crystals").innerHTML = Math.floor(gameData.crystals) + " Crystals"
-    document.getElementById("damagePerSecond").innerHTML = Math.floor(gameData.damagePerSecond) + " DPS"
-    document.getElementById("enemyHealth").innerHTML = gameData.currentEnemy.name + ": " + Math.floor(gameData.currentEnemy.hp) + "/" + Math.floor(gameData.currentEnemy.maxHp) + " HP"
-    document.getElementById("killCounter").innerHTML = gameData.zoneKillCounterKills + "/" + gameData.zoneKillCounterRequirement + " Kills to next level"
-    document.getElementById("zone").innerHTML = "Zone: " + gameData.currentZone + " (Level " + gameData.currentLevel + ")"
-    
+    document.getElementById("bones").innerHTML = Math.floor(gameData.bones) + " Bones";
+    document.getElementById("crystals").innerHTML = Math.floor(gameData.crystals) + " Crystals";
+    document.getElementById("damagePerSecond").innerHTML = Math.floor(gameData.damagePerSecond) + " DPS";
+    document.getElementById("enemyHealth").innerHTML = gameData.currentEnemy.name + ": " + Math.floor(gameData.currentEnemy.hp) + "/" + Math.floor(gameData.currentEnemy.maxHp) + " HP";
+    document.getElementById("killCounter").innerHTML = gameData.zoneKillCounterKills + "/" + gameData.zoneKillCounterRequirement + " Kills to next level";
+    document.getElementById("zone").innerHTML = "Zone: " + gameData.currentZone + " (Level " + gameData.currentLevel + ")";
+    document.getElementById("crystalDamageDisplay").innerHTML = "Current bonus: " + Math.floor(gameData.damageMultiplier * 100) / 100 + "x";
+    document.getElementById("crystalBoneDisplay").innerHTML = "Current bonus: " + Math.floor(gameData.boneMultiplier * 100) / 100 + "x";
+    document.getElementById("crystalCrystalDisplay").innerHTML = "Current chance: " + Math.floor(gameData.crystalChance) + "%";
+    document.getElementById("buyCrystalDamage").innerHTML = "Upgrade for " + Math.ceil(gameData.upgrades.damage.cost) + " crystals";
+    document.getElementById("buyCrystalBone").innerHTML = "Upgrade for " + Math.ceil(gameData.upgrades.bone.cost) + " crystals";
+    document.getElementById("buyCrystalCrystal").innerHTML = "Upgrade for " + Math.ceil(gameData.upgrades.crystal.cost) + " crystals";
 }
 
 function prevLevel() {
     if (gameData.currentLevel > 1) {
-        gameData.currentLevel--
-        spawnNewEnemy()
-        document.getElementById("killCounter").style.visibility = "hidden"
+        gameData.currentLevel--;
+        spawnNewEnemy();
+        document.getElementById("killCounter").style.visibility = "hidden";
     }
 }
 
 function nextLevel() {
     if (gameData.currentLevel == gameData.highestLevelThisGame - 1) {
-        gameData.currentLevel++
-        spawnNewEnemy()
-        document.getElementById("killCounter").style.visibility = "visible"
+        gameData.currentLevel++;
+        spawnNewEnemy();
+        document.getElementById("killCounter").style.visibility = "visible";
     }
     if (gameData.currentLevel < gameData.highestLevelThisGame - 1) {
-        gameData.currentLevel++
-        spawnNewEnemy()
+        gameData.currentLevel++;
+        spawnNewEnemy();
     }
 }
 
 function spawnNewEnemy() {
-    var lvl = gameData.currentLevel
-    var hpMultiplier = 1
-    var hpBase = 10
-    var enemyhp = Math.pow(1.5, lvl - 1) * hpMultiplier * hpBase
-    var bonusCrystalChance = 0
-    var cappedCrystalChance = gameData.crystalChance
+    var lvl = gameData.currentLevel;
+    var hpMultiplier = 1;
+    var hpBase = 10;
+    var enemyhp = Math.pow(1.5, lvl - 1) * hpMultiplier * hpBase;
+    var bonusCrystalChance = 0;
+    var cappedCrystalChance = gameData.crystalChance;
     if (gameData.crystalChance > 99) {
-        bonusCrystalChance = gameData.crystalChance - 99
-        cappedCrystalChance = 99
+        bonusCrystalChance = gameData.crystalChance - 99;
+        cappedCrystalChance = 99;
     }
     if (bonusCrystalChance > 99998) {
-        bonusCrystalChance = 99998
+        bonusCrystalChance = 99998;
     }
-    var enemyType = 0
-    if (lvl%5 == 0) {
-        enemyType = 1
+    var enemyType = 0;
+    if (lvl % 5 == 0) {
+        enemyType = 1;
     }
-    var enemyName = enemyNames[Math.floor(Math.random() * enemyNames.length)]
+    var enemyName = enemyNames[Math.floor(Math.random() * enemyNames.length)];
     if (enemyType == 1) {
-        enemyName = bossNames.oozingForest[Math.floor(Math.random() * bossNames.oozingForest.length)]
+        enemyName = bossNames.oozingForest[Math.floor(Math.random() * bossNames.oozingForest.length)];
     }
-    var rar1 = Math.floor(Math.random() * (1 / (1 - cappedCrystalChance/100)))
-    var rar2 = Math.floor(Math.random() * (1 / (1 - ((0.00001) * (bonusCrystalChance + 1)))))
+    var rar1 = Math.floor(Math.random() * (1 / (1 - cappedCrystalChance / 100)));
+    var rar2 = Math.floor(Math.random() * (1 / (1 - ((0.00001) * (bonusCrystalChance + 1)))));
 
     if (rar1 >= 1) {
-        rar1 = 1
-        enemyName = "Crystallized " + enemyName
+        rar1 = 1;
+        enemyName = "Crystallized " + enemyName;
     }
     if (rar2 >= 1) {
-        rar2 = 1
-        enemyName = "Elite " + enemyName
+        rar2 = 1;
+        enemyName = "Elite " + enemyName;
     }
 
-    console.log("" + cappedCrystalChance + " " + bonusCrystalChance)
+    console.log("" + cappedCrystalChance + " " + bonusCrystalChance);
 
     gameData.currentEnemy = {
         hp: Math.floor(enemyhp * (1 + (9 * enemyType))),
@@ -181,173 +186,192 @@ function spawnNewEnemy() {
         rarity2: rar2,
         type: enemyType,
         name: enemyName
-    }
+    };
 }
 
 function giveDrops(enemy) {
-    var bones = 0
-    var crystals = 0
+    var bones = 0;
+    var crystals = 0;
 
-    bones = Math.ceil(enemy.maxHp / 5 * (1 + (enemy.rarity * 5)) * (1 + (enemy.rarity2 * 5))) * gameData.boneMultiplier
-    crystals = Math.ceil(enemy.rarity * (enemy.maxHp / 15) * (1 + (enemy.rarity2 * 5)))
+    bones = Math.ceil(enemy.maxHp / 5 * (1 + (enemy.rarity * 5)) * (1 + (enemy.rarity2 * 5))) * gameData.boneMultiplier;
+    crystals = Math.ceil(enemy.rarity * (enemy.maxHp / 15) * (1 + (enemy.rarity2 * 5)));
 
-    gameData.bones += bones
-    gameData.crystals += crystals
+    gameData.bones += bones;
+    gameData.crystals += crystals;
 }
 
 function attackMonster() {
-    enemy = gameData.currentEnemy
-    enemy.hp -= gameData.damagePerSecond / 15
+    enemy = gameData.currentEnemy;
+    enemy.hp -= gameData.damagePerSecond / 15;
     if (enemy.hp <= 0) {
-        gameData.totalKills++
-        giveDrops(enemy)
-        spawnNewEnemy()
+        gameData.totalKills++;
+        giveDrops(enemy);
+        spawnNewEnemy();
         if (gameData.currentLevel == gameData.highestLevelThisGame && gameData.zoneKillCounterKills < gameData.zoneKillCounterRequirement) {
-            gameData.zoneKillCounterKills++
+            gameData.zoneKillCounterKills++;
         }
         if (gameData.zoneKillCounterKills >= gameData.zoneKillCounterRequirement) {
-            gameData.highestLevelThisGame++
-            gameData.zoneKillCounterKills = 0
-            if (gameData.highestLevelThisGame%5==0) {
-                gameData.zoneKillCounterRequirement = 1
+            gameData.highestLevelThisGame++;
+            gameData.zoneKillCounterKills = 0;
+            if (gameData.highestLevelThisGame % 5 == 0) {
+                gameData.zoneKillCounterRequirement = 1;
             }
             else if (gameData.zoneKillCounterRequirement = 1) {
-                gameData.zoneKillCounterRequirement = 10
+                gameData.zoneKillCounterRequirement = 10;
             }
             document.getElementById("killCounter").style.visibility = "hidden";
             if (gameData.autoZoneProgression) {
-                gameData.currentLevel++
-                spawnNewEnemy()
-                document.getElementById("killCounter").style.visibility = "visible"
+                gameData.currentLevel++;
+                spawnNewEnemy();
+                document.getElementById("killCounter").style.visibility = "visible";
             }
         }
     }
 }
 
 function recalculateDPS() {
-    var dps = 0
-    dinos = gameData.dinos
-    dino1 = dinos.dino1
-    dino2 = dinos.dino2
-    dino3 = dinos.dino3
-    dino4 = dinos.dino4
-    dino5 = dinos.dino5
+    var dps = 0;
+    var dinos = gameData.dinos;
+    var dino1 = dinos.dino1;
+    var dino2 = dinos.dino2;
+    var dino3 = dinos.dino3;
+    var dino4 = dinos.dino4;
+    var dino5 = dinos.dino5;
 
-    calculateDinoDPS(dino1)
-    dps += dino1.dinoDps
-    calculateDinoDPS(dino2)
-    dps += dino2.dinoDps
-    calculateDinoDPS(dino3)
-    dps += dino3.dinoDps
-    calculateDinoDPS(dino4)
-    dps += dino4.dinoDps
-    calculateDinoDPS(dino5)
-    dps += dino5.dinoDps
+    calculateDinoDPS(dino1);
+    dps += dino1.dinoDps;
+    calculateDinoDPS(dino2);
+    dps += dino2.dinoDps;
+    calculateDinoDPS(dino3);
+    dps += dino3.dinoDps;
+    calculateDinoDPS(dino4);
+    dps += dino4.dinoDps;
+    calculateDinoDPS(dino5);
+    dps += dino5.dinoDps;
 
-    gameData.damagePerSecond = dps
+    gameData.damagePerSecond = dps;
 }
 
 function calculateDinoDPS(dino) {
-    dino.multiplier = 1 * Math.pow(3, Math.floor(dino.quantity / 10)) * gameData.damageMultiplier
-    dino.dinoDps = dino.baseDps * dino.quantity * dino.multiplier
-    document.getElementById(dino.nameString + "dps").innerHTML = dino.nameString + " - " + Math.floor(dino.dinoDps) + " DPS - Level " + dino.quantity
-    document.getElementById(dino.nameString + "DamageBonus").innerHTML = "Next damage bonus at level " + Math.ceil(dino.quantity / 10) * 10
+    dino.multiplier = 1 * Math.pow(3, Math.floor(dino.quantity / 10)) * gameData.damageMultiplier;
+    dino.dinoDps = dino.baseDps * dino.quantity * dino.multiplier;
+    document.getElementById(dino.nameString + "dps").innerHTML = dino.nameString + " - " + Math.floor(dino.dinoDps) + " DPS - Level " + dino.quantity;
+    document.getElementById(dino.nameString + "DamageBonus").innerHTML = "Next damage bonus at level " + Math.ceil((dino.quantity+1) / 10) * 10;
+    document.getElementById("buy" + dino.nameString).innerHTML = "Upgrade " + dino.nameString + " for " + Math.ceil(dino.cost) + " Bones";
 }
 
 function buyDino(dinoName) {
-    dino = gameData.dinos[dinoName]
+    dino = gameData.dinos[dinoName];
     if (gameData.bones >= dino.cost) {
-        gameData.bones -= dino.cost
-        dino.quantity++
-        dino.cost *= dino.scaling
-        document.getElementById("buy" + dinoName).innerHTML = "Upgrade " + dinoName + " for " + Math.ceil(dino.cost) + " Bones"
+        gameData.bones -= dino.cost;
+        dino.quantity++;
+        dino.cost *= dino.scaling;
         recalculateDPS()
     }
 }
 
 function buyCrystalUpgrade(upgradeName) {
-    upgrade = gameData.upgrades[upgradeName]
+    var upgrade = gameData.upgrades[upgradeName];
     if (gameData.crystals >= upgrade.cost) {
-        gameData.crystals -= upgrade.cost
-        upgrade.level++
-        upgrade.cost = upgrade.baseCost * Math.pow(upgrade.scaling, upgrade.level)
+        gameData.crystals -= upgrade.cost;
+        upgrade.level++;
+        upgrade.cost = upgrade.baseCost * Math.pow(upgrade.scaling, upgrade.level);
     }
-    gameData.boneMultiplier = 1 * Math.pow(gameData.upgrades.bone.power, gameData.upgrades.bone.level-1)
-    gameData.damageMultiplier = 1 * Math.pow(gameData.upgrades.damage.power, gameData.upgrades.damage.level-1)
-    gameData.crystalChance = (2 + (gameData.upgrades.crystal.level * gameData.upgrades.crystal.power))
-    recalculateDPS()
-    document.getElementById("crystalDamageDisplay").innerHTML = "Current bonus: " + Math.floor(gameData.damageMultiplier * 100)/100 + "x"
-    document.getElementById("crystalBoneDisplay").innerHTML = "Current bonus: " + Math.floor(gameData.boneMultiplier * 100)/100 + "x"
-    document.getElementById("crystalCrystalDisplay").innerHTML = "Current chance: " + Math.floor(gameData.crystalChance) + "%"
-    document.getElementById("buyCrystalDamage").innerHTML = "Upgrade for " + Math.ceil(gameData.upgrades.damage.cost) + " crystals"
-    document.getElementById("buyCrystalBone").innerHTML = "Upgrade for " + Math.ceil(gameData.upgrades.bone.cost) + " crystals"
-    document.getElementById("buyCrystalCrystal").innerHTML = "Upgrade for " + Math.ceil(gameData.upgrades.crystal.cost) + " crystals"
+    gameData.boneMultiplier = 1 * Math.pow(gameData.upgrades.bone.power, gameData.upgrades.bone.level - 1);
+    gameData.damageMultiplier = 1 * Math.pow(gameData.upgrades.damage.power, gameData.upgrades.damage.level - 1);
+    gameData.crystalChance = (2 + (gameData.upgrades.crystal.level * gameData.upgrades.crystal.power));
+    recalculateDPS();
 }
-var r = 255
-var g = 0
-var b = 255
-var step = 1
+var r = 255;
+var g = 0;
+var b = 255;
+var step = 1;
 function rainbowtexttest() {
     if (step == 1) {
         if (b < 1) {
-            step++
+            step++;
         }
         else {
-            b--
-            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")"
+            b--;
+            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")";
         }
     }
     if (step == 2) {
         if (g > 254) {
-            step++
+            step++;
         }
         else {
-            g++
-            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")"
+            g++;
+            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")";
         }
     }
     if (step == 3) {
         if (r < 1) {
-            step++
+            step++;
         }
         else {
-            r--
-            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")"
+            r--;
+            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")";
         }
     }
     if (step == 4) {
         if (b > 254) {
-            step++
+            step++;
         }
         else {
-            b++
-            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")"
+            b++;
+            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")";
         }
     }
     if (step == 5) {
         if (g < 1) {
-            step++
+            step++;
         }
         else {
-            g--
-            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")"
+            g--;
+            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")";
         }
     }
     if (step == 6) {
         if (r > 254) {
-            step = 1
+            step = 1;
         }
         else {
-            r++
-            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")"
+            r++;
+            document.getElementById("rainbowtexttest").style.color = "rgb(" + r + "," + g + "," + b + ")";
         }
     }
-    setTimeout(rainbowtexttest, 5)
+    setTimeout(rainbowtexttest, 5);
 }
 
+function autoSave() {
+    localStorage.setItem('gameData', JSON.stringify(gameData));
+}
 
+function loadData() {
+    var retrievedObject = localStorage.getItem('gameData');
+    if (retrievedObject != null) {
+        gameData = JSON.parse(retrievedObject);
+        console.log(retrievedObject);
+    }
+    else {
+        localStorage.setItem('gameData', JSON.stringify(gameData));
+    }
+}
 
-setInterval(update, 1000 / 60)
-setInterval(attackMonster, 1000 / 15)
-rainbowtexttest()
-recalculateDPS()
+function startLoading() {
+    setInterval(update, 1000 / 60);
+    setInterval(attackMonster, 1000 / 15);
+    rainbowtexttest();
+    recalculateDPS();
+    setInterval(autoSave, 15000)
+}
+
+window.onbeforeunload = closingCode;
+function closingCode() {
+    autoSave();
+    return null;
+}
+
+loadData();
+setTimeout(startLoading, 100);
